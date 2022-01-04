5ed736753c4057760867d7f98373950b588a1c5d
Chris Stewart <mils8545@gmail.com>
2022-01-03 17:15:32 -0700

    Day 19: Regex Madness

--- 19/app.js
+++ 19/app.js
@@ -7,44 +7,98 @@ const readFile = (readFile) => {
   return lines;
 };
 
-const ex1 = (file) => {
-  const lines = readFile(file);
-
+const parseLines = (lines) => {
   let dict = {};
-
+  let words = [];
   let i = 0;
   while (lines[i] != "") {
     const line = lines[i];
-    dict[line.slice(0, line.indexOf(":"))] = line.slice(line.indexOf(":")+2).split(" | ").map(entry => entry.split(" "));
+    dict[line.slice(0, line.indexOf(":"))] = "("+line.slice(line.indexOf(":")+2).split(" ").join(")(").replaceAll("(|)", "|").replaceAll('"a"', "a").replaceAll('"b"', "b") +")";
     i++;
   }
 
-  let queue = [];
-  let valid = [];
-  queue.push(dict['0'][0]);
-  while (queue.length > 0) {
-    let current = queue.shift();
-    if (current[current.length-1] == '"a"' || current[current.length-1] == '"b"') {
-      valid.push(current);
-    } else {
-      let rIndex = -1;
-      let i = 0;
-      while (rIndex < 0) {
-        if (!(current[i] == '"a"' || current[i] == '"b"')) rIndex = i;
-        i++;
+  i++;
+
+  while (i < lines.length) {
+    words.push(lines[i]);
+    i++;
+  }
+
+  return {dict: dict, words: words};
+}
+
+const ex1 = (file) => {
+  const lines = readFile(file);
+
+  let {dict, words} = parseLines(lines);
+
+  let bigRule = dict[0];
+  let parsed = false;
+  let i = 0;
+  while (!parsed) {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    if (digits.includes(bigRule[i])) {
+      let j = 1;
+      while (digits.includes(bigRule[i+j])) {
+        j++;
       }
-      dict[current[rIndex]].forEach(br => {
-        queue.push([...current.slice(0, rIndex), br, ...current.slice(rIndex+1)].flat());
-      });
+      bigRule = bigRule.slice(0,i) + dict[bigRule.slice(i,i+j)] + bigRule.slice(i+j);
+      i = 0;
+    }
+    i++;
+    if (i >= bigRule.length) {
+      parsed = true;
     }
   }
-  valid = valid.map(line => line.map(e => e[1]).join(""));
-  console.log(valid);
 
-//  console.log(`EX 19-1: The total of all problems is ${total}.`);
+  bigRule = "^" + bigRule + "$";
+
+  let re = new RegExp(bigRule)
+
+  let count = words.filter(word => re.test(word)).length;
+
+  console.log(`EX 19-1: ${count} entries match the rules.`);
 };
 
 const ex2 = (file) => {
+  const lines = readFile(file);
+
+  let {dict, words} = parseLines(lines);
+
+  let longrule = "";
+  for (let i = 1; i < 5; i++) {
+    longrule += "(42)".repeat(i) + "(31)".repeat(i) + "|"; 
+  }
+
+  dict[11] = longrule.slice(0, -1);
+  dict[8] = "(42)+";
+
+  let bigRule = dict[0];
+  let parsed = false;
+  let i = 0;
+  while (!parsed) {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    if (digits.includes(bigRule[i])) {
+      let j = 1;
+      while (digits.includes(bigRule[i+j])) {
+        j++;
+      }
+      bigRule = bigRule.slice(0,i) + dict[bigRule.slice(i,i+j)] + bigRule.slice(i+j);
+      i = 0;
+    }
+    i++;
+    if (i >= bigRule.length) {
+      parsed = true;
+    }
+  }
+
+  bigRule = "^" + bigRule + "$";
+
+  let re = new RegExp(bigRule)
+
+  let count = words.filter(word => re.test(word)).length;
+
+  console.log(`EX 19-2: ${count} entries match the rules.`);
 };
 
 let startTime = performance.now();
